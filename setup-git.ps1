1a024d2e04320bc9a47a4485eca2b551463a949d
Random-Simulation <user@example.com>
2026-07-14 09:24:51 +0100

    Remove setup script

--- setup-git.ps1
+++
@@ -1,6 +0,0 @@
-$remoteUrl = "https://github.com/Random-Simulation/Bare.git"
-git remote add origin $remoteUrl
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git push -u origin main
